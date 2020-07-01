57bfc8298b765a58eda39164ae213677ae2d627b
Simone Biffi <simone.biffi.91@gmail.com>
2020-07-01 12:06:49 +0200

    feat(ex2): create array of promises

--- chartCrypto.js
+++ chartCrypto.js
@@ -2,9 +2,27 @@
 // https://www.alphavantage.co/documentation (closing daily US con USD market)
 const moment = require("moment") //https://momentjs.com/docs/
 const exampleData = require("./exampleDataCrypto.json")
+
 module.exports.default = function (req, res, next) {
     var rp = require('request-promise'); //https://www.npmjs.com/package/request-promise
+    const coins = ['BTC', 'LTC', 'ETH'];
+    const apiKey = 'J06QACM0IZY7CCHY';
+
+    let coinsPromises = [];
+    coins.map(coin => {
+        var coin_details = {
+            uri: `https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&market=CNY`,
+            qs: {
+                symbol: coin,
+                apikey: apiKey
+            },
+            json: true // Automatically parses the JSON string in the response
+        };
+        coinsPromises.push(rp(coin_details));
+    });
+
+
     const formattedData = {}
-            res.send(exampleData);
+    res.send(exampleData);
 }
 
